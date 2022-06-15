2518ef46ed7378ace779bac72c6702c4f94a4d47
FinderV <kavalevaviktoryia@gmail.com>
2022-06-15 20:45:58 +0300

    add

--- src/test/java/org/tms/test/BaseTest.java
+++ src/test/java/org/tms/test/BaseTest.java
@@ -1,6 +1,5 @@
 package org.tms.test;
 
-import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -8,7 +7,6 @@ import org.testng.annotations.Listeners;
 import org.tms.driver.DriverSingleton;
 import org.tms.utils.TestListener;
 
-@Log4j2
 @Listeners(TestListener.class)
 public class BaseTest {
 
